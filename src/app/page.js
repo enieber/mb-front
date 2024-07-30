06cee6fabe9d640427d80b1c4816d28609d1eff1
Enieber Cunha <eniebercunha@gmail.com>
2024-07-30 03:27:03 -0300

    feat: add assets from site

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,61 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className={styles.main}>                  
+      <Image
+        src="/home.png"
+        alt="casas de bairros"
+        className={styles.home}
+        width={300}
+        height={300}
+        priority
+      />
+      <h2>Conectando Pessoas seus bairos</h2>
+      <Image
+        src="/products.png"
+        alt="Curve"
+        className={styles.curve}
+        width={300}
+        height={300}
+        priority
+      />
+      <h2>O que resolvemos?</h2>
+      <p>Você se muda para um novo bairro e percebe que precisa de uma lâmpada nova, mas não sabe onde comprar? O Projeto Meu Bairro está aqui para ajudar você! Somos plataforma online que mostra tudo o que está disponível proximo de você.</p>
+      <h2>Produtos Locais</h2>
+      <Image
+        src="/call.png"
+        alt="casas de bairros"
+        className={styles.home}
+        width={300}
+        height={300}
+        priority
+      />
+      <p>Com o Meu Bairro será possivel visualizar e pesquisar produtos dentro do seu bairro</p>
+      <h2>Serviços Proximo</h2>
+      <Image
+        src="/home.png"
+        alt="casas de bairros"
+        className={styles.home}
+        width={300}
+        height={300}
+        priority
+      />
+      <p>
+        Com o Meu Bairro você poderar oferecer seus serviços ou encontrar um profissional proximo a você.
+      </p>
+      <h2>Federação</h2>
+      <Image
+        src="/graphics.png"
+        alt="casas de bairros"
+        className={styles.home}
+        width={300}
+        height={300}
+        priority
+      />
+      <p>
+        Os usuarios do Meu Bairro podem consumir produtos e serviços dos outros bairros atravez o uso de uma rede federada onde o acesso vai ser como pegar um onibus e ir até o outro bairro, mas isso sem precisar sair de casa.
+      </p>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
